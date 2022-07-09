702cfc6ce5bd51e0792e6df388d37cd65d710c7c
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-09 18:00:38 +0530

    complete project

--- js/index.js
+++ js/index.js
@@ -5,14 +5,28 @@ range.addEventListener('input', (e) => {
   const label = e.target.nextElementSibling
 
   const range_width = getComputedStyle(e.target).getPropertyValue('width')
+  const label_width = getComputedStyle(label).getPropertyValue('width')
 
-  
+  const num_width = +range_width.substring(0,
+  range_width.length - 2) 
+  const num_lable_width = +label_width.substring(0,
+  label_width.length - 2)
+ 
+  const max = +e.target.max
+  const min = +e.target.min
+
+  const left = value * (num_width / max) - num_lable_width / 2 + scale(value, min, max, 10, -10)
+
+  label.style.left = `${left}px`
+
+  console.log(left)
 
   label.innerHTML = value
 })
 
-
-
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 
 
