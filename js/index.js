aa296896788a03a620b3a064df7ac79883f53131
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-09 17:22:07 +0530

    make function

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const range = document.getElementById('range')
+
+range.addEventListener('input', (e) => {
+  const value = +e.target.value
+  const label = e.target.nextElementSibling
+
+  const range_width = getComputedStyle(e.target).getPropertyValue('width')
+
+  
+
+  label.innerHTML = value
+})
+
+
+
+
+
+
+
+
